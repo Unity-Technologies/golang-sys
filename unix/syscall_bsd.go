c1419fef1fbac6ad773d8ab46ae5749c457802bb
Steven Hartland <steven.hartland@multiplay.co.uk>
2015-02-01 16:04:03 +0000

    Add custom Sysctl args and methods SysctlUint64, SysctlRaw
    Add support for optional sysctl arguments which is required to support sysctls that require more than the mib identifer args as returned from nametomib such as kern.proc.pid.
    
    Add SysctlUint64 which allows sysctls that return 64 bit ints to be queried.
    
    Add SysctlRaw which allows sysctls that return structs or other unsupported types to be queried.

--- unix/syscall_bsd.go
+++ unix/syscall_bsd.go
@@ -450,12 +450,22 @@ func Kevent(kq int, changes, events []Kevent_t, timeout *Timespec) (n int, err e
 
 //sys	sysctl(mib []_C_int, old *byte, oldlen *uintptr, new *byte, newlen uintptr) (err error) = SYS___SYSCTL
 
-func Sysctl(name string) (value string, err error) {
+// sysctlArgs adds any optional args to mib
+func sysctlArgs(mib []_C_int, args ...int) []_C_int {
+	for _, a := range args {
+		mib = append(mib, _C_int(a))
+	}
+	return mib
+}
+
+func Sysctl(name string, args ...int) (value string, err error) {
 	// Translate name to mib number.
 	mib, err := nametomib(name)
 	if err != nil {
 		return "", err
 	}
+
+	mib = sysctlArgs(mib, args...)
 
 	// Find size.
 	n := uintptr(0)
@@ -479,12 +489,14 @@ func Sysctl(name string) (value string, err error) {
 	return string(buf[0:n]), nil
 }
 
-func SysctlUint32(name string) (value uint32, err error) {
+func SysctlUint32(name string, args ...int) (value uint32, err error) {
 	// Translate name to mib number.
 	mib, err := nametomib(name)
 	if err != nil {
 		return 0, err
 	}
+
+	mib = sysctlArgs(mib, args...)
 
 	// Read into buffer of that size.
 	n := uintptr(4)
@@ -496,6 +508,55 @@ func SysctlUint32(name string) (value uint32, err error) {
 		return 0, EIO
 	}
 	return *(*uint32)(unsafe.Pointer(&buf[0])), nil
+}
+
+func SysctlUint64(name string, args ...int) (value uint64, err error) {
+	// Translate name to mib number.
+	mib, err := nametomib(name)
+	if err != nil {
+		return 0, err
+	}
+
+	mib = sysctlArgs(mib, args...)
+
+	// Read into buffer of that size.
+	n := uintptr(8)
+	buf := make([]byte, 8)
+	if err = sysctl(mib, &buf[0], &n, nil, 0); err != nil {
+		return 0, err
+	}
+	if n != 8 {
+		return 0, EIO
+	}
+
+	return *(*uint64)(unsafe.Pointer(&buf[0])), nil
+}
+
+func SysctlRaw(name string, args ...int) (buf []byte, err error) {
+	// Translate name to mib number.
+	mib, err := nametomib(name)
+	if err != nil {
+		return buf, err
+	}
+
+	mib = sysctlArgs(mib, args...)
+
+	// Find size.
+	n := uintptr(0)
+	if err = sysctl(mib, nil, &n, nil, 0); err != nil {
+		return buf, err
+	}
+	if n == 0 {
+		return buf, nil
+	}
+
+	// Read into buffer of that size.
+	buf = make([]byte, n)
+	err = sysctl(mib, &buf[0], &n, nil, 0)
+
+	// The actual call may return less than the original reported required
+	// size so ensure we deal with that
+	return buf[0:n], err
 }
 
 //sys	utimes(path string, timeval *[2]Timeval) (err error)
